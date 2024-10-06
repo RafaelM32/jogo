4516a16ce9d6aa0871744c8f4ea2dea980e82ac8
Rafael <raphaelmenezesvill@gmail.com>
2024-10-06 09:35:21 -0300

    criando animacao do personagem

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const teste = document.getElementById("hora")
+let contador = 0
+var FPS = 30
+
+
+class Player{
+
+    parado = true
+    andando = false
+    direita = true
+    esquerda = false
+
+    animacoes = {
+        andar_direita:"",
+        andar_esquerda:""
+    }
+}
+
