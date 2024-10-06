b4aca90c62e96a3098cdda6b0a64740bcbbf4820
Rafael <raphaelmenezesvill@gmail.com>
2024-10-06 17:22:52 -0300

    adicionando a logica do player

--- script/script.js
+++ script/script.js
@@ -1,35 +1,40 @@
 const teste = document.getElementById("hora")
 let contador = 0
-var FPS = 30
+var FPS = 13
 
 class Player{
-
-    imagem_atual;
-    imagem_padrao;
-    elemento_html;
-    
-    estado = {
-        direita: true,
-        esquerda: false,
-        andando: false,
-    }
-    animacoes = {
-        andar_direita:"img/andar/direita",
-        andar_esquerda:""
-    }
 
     constructor(imagem_padrao, elemento_html){
         this.imagem_atual  = imagem_padrao
         this.imagem_padrao = imagem_padrao
         this.elemento_html = elemento_html
+        this.animacoes ={
+            andar_direita:{ diretorio: "img/andar/direita/", numero_da_imagem_atual: 0}
+        }
     }
+
 
     mudar_imagem(img){
         this.elemento_html.src = img
+        this.imagem_atual = img
     }
 
+    andar_para_direita(t){
+        const animacao = t.animacoes.andar_direita
+        const img = animacao.diretorio+animacao.numero_da_imagem_atual+".png"
+        t.mudar_imagem(img)
+        animacao.numero_da_imagem_atual += 1
+        if(animacao.numero_da_imagem_atual == 6){
+            animacao.numero_da_imagem_atual=0
+        }
+    }
+
+    andar(){
+        setInterval(this.andar_para_direita,1000/FPS,this)
+    }
     
 }
 
-const player = new Player("img/player_0r.png",document.getElementById("player"))
 
+
+const player = new Player("img/andar/direita/0.png",document.getElementById("player"))
