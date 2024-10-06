72ac07116ce09e0a96dc057fbff3d7f36cf19d60
Rafael <raphaelmenezesvill@gmail.com>
2024-10-06 10:48:44 -0300

    iniciando a logica de animação do jogador

--- script/script.js
+++ script/script.js
@@ -2,17 +2,34 @@ const teste = document.getElementById("hora")
 let contador = 0
 var FPS = 30
 
-
 class Player{
 
-    parado = true
-    andando = false
-    direita = true
-    esquerda = false
-
+    imagem_atual;
+    imagem_padrao;
+    elemento_html;
+    
+    estado = {
+        direita: true,
+        esquerda: false,
+        andando: false,
+    }
     animacoes = {
-        andar_direita:"",
+        andar_direita:"img/andar/direita",
         andar_esquerda:""
     }
+
+    constructor(imagem_padrao, elemento_html){
+        this.imagem_atual  = imagem_padrao
+        this.imagem_padrao = imagem_padrao
+        this.elemento_html = elemento_html
+    }
+
+    mudar_imagem(img){
+        this.elemento_html.src = img
+    }
+
+    
 }
+
+const player = new Player("img/player_0r.png",document.getElementById("player"))
 
